69d16edee8b7fe47ee849f4b5849080b54044fca
Kaue-Dev <kauemassola900@gmail.com>
2025-03-26 19:28:53 -0300

    feat: implements first section layout object

---
+++ src/components/first-section-layout/index.ts
@@ -0,0 +1,15 @@
+import { Root } from './root';
+import { Header } from './header';
+import { Title } from './title';
+import { SideImage } from './side-image';
+import { Footer } from './footer';
+import { Wrapper } from './wrapper';
+
+export const FirstSectionLayout = {
+  Root: Root,
+  Header: Header,
+  Title: Title,
+  SideImage: SideImage,
+  Footer: Footer,
+  Wrapper: Wrapper,
+}
