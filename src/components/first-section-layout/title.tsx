13a7ff79bfafd183ab295360b16e90ad410709b5
Kaue-Dev <kauemassola900@gmail.com>
2025-03-26 19:27:21 -0300

    feat: implements title component

---
+++ src/components/first-section-layout/title.tsx
@@ -0,0 +1,8 @@
+export const Title = () => {
+  return (
+    <div className="w-fit mx-auto">
+      <h1 className="font-arizonia text-center text-[152px] font-normal text-bluebell-blue mb-2">Cascavel</h1>
+      <p className=" font-montserrat text-center text-[32px] font-normal">Propriedades ideais para <span className="font-medium">viver</span> e <span className="font-medium">investir</span>.</p>
+    </div>
+  )
+}
