a0f3729c304da4aa5ec105a5d9f7241c7e6e3133
Kaue-Dev <kauemassola900@gmail.com>
2025-03-31 14:38:04 -0300

    feat: add styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { SecondSection } from "@/components/second-section";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center w-full gap-8">
       <FirstSection />
       <SecondSection />
-    </>
+    </div>
   )
 }
