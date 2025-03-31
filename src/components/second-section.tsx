9f811188128c3af250f99fc6d0e32fec887ff16d
Kaue-Dev <kauemassola900@gmail.com>
2025-03-31 15:39:42 -0300

    feat: implements SecondSection component

---
+++ src/components/second-section.tsx
@@ -0,0 +1,19 @@
+import { Logo } from "./global/logo"
+import { SecondSectionLayout } from "./second-section-layout"
+
+export const SecondSection = () => {
+  return (
+    <SecondSectionLayout.Root>
+      <SecondSectionLayout.Wrapper>
+        <div className="flex flex-col items-start justify-start gap-4 w-full">
+          <Logo />
+          <p className="text-3xl font-medium text-bluebell-blue font-montserrat max-w-[740px] text-justify">
+            Aqui na Bluebell, nós oferecemos mais do que serviços imobiliários, nós oferecemos uma <span className="font-semibold">experência única</span> com o objetivo de satisfazer suas necessidades e ultrapassar suas expectativas.
+          </p>
+        </div>
+        <SecondSectionLayout.Statistics />
+      </SecondSectionLayout.Wrapper>
+      <SecondSectionLayout.SideImage />
+    </SecondSectionLayout.Root>
+  )
+}
