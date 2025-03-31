7f501b2daec6c90c5f5053fd393c40dc9444f84f
Kaue-Dev <kauemassola900@gmail.com>
2025-03-31 15:39:22 -0300

    feat: implements SecondSectionLayout

---
+++ src/components/second-section-layout/index.ts
@@ -0,0 +1,11 @@
+import { Root } from './root';
+import { SideImage } from './side-image';
+import { Statistics } from './statistics';
+import { Wrapper } from './wrapper';
+
+export const SecondSectionLayout = {
+  Root: Root,
+  SideImage: SideImage,
+  Wrapper: Wrapper,
+  Statistics: Statistics,
+}
