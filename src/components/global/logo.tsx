98ecef38f104aec657d95852229e957cba12d977
Kaue-Dev <kauemassola900@gmail.com>
2025-03-31 14:11:06 -0300

    refactor: add h-fit and w-auto

--- src/components/global/logo.tsx
+++ src/components/global/logo.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 import LogoImage from "@/assets/images/logo.png";
 
 export const Logo = () => {
-  return <Image src={LogoImage} alt="Logo Bluebell" />
+  return <Image src={LogoImage} alt="Logo Bluebell" className="h-fit w-auto" />
 }
