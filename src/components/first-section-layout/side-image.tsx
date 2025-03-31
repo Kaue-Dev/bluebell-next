373edd039693d409b244de36a423348cc0951b51
Kaue-Dev <kauemassola900@gmail.com>
2025-03-31 13:51:56 -0300

    feat: add new btn

--- src/components/first-section-layout/side-image.tsx
+++ src/components/first-section-layout/side-image.tsx
@@ -1,6 +1,16 @@
 import Image from "next/image";
 import Image1 from "@/assets/images/image-1.png";
+import { Button } from "../ui/button";
+import { CloudDownload } from "lucide-react";
+import Link from "next/link";
 
 export function SideImage() {
-  return <Image src={Image1} alt="Side Image" className="object-cover h-full w-full rounded-2xl" />;
+  return (
+    <div className="w-full h-full rounded-2xl overflow-hidden relative">
+      <Image src={Image1} alt="Side Image" className="object-cover h-full w-full" />
+      <Link href="/" target="_blank" className="absolute bottom-4 right-4 cursor-pointer z-50">
+        <Button variant="default">Baixar nosso catálogo <CloudDownload size={16} /></Button>
+      </Link>
+    </div>
+  )
 }
