85290eb81ed057d225ef322db602c4ff59e37d5e
Kaue-Dev <kauemassola900@gmail.com>
2025-03-31 14:10:05 -0300

    feat: add second section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { FirstSection } from "@/components/first-section";
+import { SecondSection } from "@/components/second-section";
 
 export default function Home() {
   return (
     <>
       <FirstSection />
+      <SecondSection />
     </>
   )
 }
