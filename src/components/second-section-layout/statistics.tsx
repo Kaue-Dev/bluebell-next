9fdec558382ae1838586cf9fa9a461e467b2f62e
Kaue-Dev <kauemassola900@gmail.com>
2025-04-02 13:36:58 -0300

    refactor: add key

--- src/components/second-section-layout/statistics.tsx
+++ src/components/second-section-layout/statistics.tsx
@@ -22,13 +22,13 @@ export const Statistics = () => {
   return (
     <div className="flex flex-col items-start justify-start gap-4 w-full mt-auto">
       {statisticsTextData.map((item, index) => (
-        <>
-          <div className="flex items-center justify-start gap-4 w-full" key={index}>
+        <div className="w-full" key={index}>
+          <div className="flex items-center justify-start gap-4 w-full">
             <div className="w-fit text-bluebell-gold font-bold text-8xl">{item.value}</div>
             <p className="w-full max-w-56 font-medium text-2xl mx-auto">{item.text}</p>
           </div>
           {index !== statisticsTextData.length - 1 && <div className="w-full h-0.5 bg-slate-300 my-4"></div>}
-        </>
+        </div>
       ))}
     </div>
   );
