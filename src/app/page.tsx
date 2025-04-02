1f70e9352da111c8f44aa9d9b13aa284c1deecab
Kaue-Dev <kauemassola900@gmail.com>
2025-04-02 15:33:32 -0300

    feat: add third section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { FirstSection } from "@/components/first-section";
 import { SecondSection } from "@/components/second-section";
+import { ThirdSection } from "@/components/third-section";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full gap-8 p-8">
       <FirstSection />
       <SecondSection />
+      <ThirdSection />
     </div>
   )
 }
