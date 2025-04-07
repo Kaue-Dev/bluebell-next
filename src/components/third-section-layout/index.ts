152e5b7375b380a82e94cc89af51bfa746418fe1
Kaue-Dev <kauemassola900@gmail.com>
2025-04-07 13:55:48 -0300

    feat: implements index.ts

---
+++ src/components/third-section-layout/index.ts
@@ -0,0 +1,13 @@
+import { Footer } from './footer';
+import { Gallery } from './gallery';
+import { GalleryItem } from './gallery-item';
+import { Header } from './header';
+import { Root } from './root';
+
+export const ThirdSectionLayout = {
+  Root: Root,
+  Header: Header,
+  Gallery: Gallery,
+  GalleryItem: GalleryItem,
+  Footer: Footer,
+}
