ba685992dbe6f73bdd040b3a3c5fbd2c91f655a5
Kaue-Dev <kaueandradepadilha@gmail.com>
2025-04-18 14:07:01 -0300

    refactor: add props

--- src/components/fourth-section-layout/card.tsx
+++ src/components/fourth-section-layout/card.tsx
@@ -1,11 +1,19 @@
-import { BadgeDollarSign } from "lucide-react"
+interface CardProps {
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+}
 
-export const Card = () => {
+export const Card = ({ 
+  icon,
+  title,
+  description
+}: CardProps) => {
   return (
-    <div className="bg-white rounded-2xl border border-slate-200 p-6">
-      <BadgeDollarSign size={36} className="text-bluebell-gold mb-2" />
-      <h3 className="font-montserrat font-semibold text-bluebell-blue/80 text-2xl text-start mb-6">Alto potencial de valorização</h3>
-      <p className="font-normal text-sm text-bluebell-blue/50 text-start w-full h-fit">A expansão urbana e o crescimento econômico da região indicam que os imóveis em Cascavel tendem a valorizar com o tempo, maximizando o retorno sobre o investimento. A expansão urbana e o crescimento econômico da região indicam que os imóveis em Cascavel tendem a valorizar com o tempo, maximizando o retorno sobre o investimento.</p>
+    <div className="bg-white rounded-2xl border border-slate-200 p-6 h-80">
+      <div className="mb-2 text-bluebell-gold">{icon}</div>
+      <h3 className="font-montserrat font-semibold text-bluebell-blue/80 text-2xl text-start mb-6 max-w-72">{title}</h3>
+      <p className="font-normal text-sm text-bluebell-blue/50 text-start w-full h-fit">{description}</p>
     </div>
   )
 }
